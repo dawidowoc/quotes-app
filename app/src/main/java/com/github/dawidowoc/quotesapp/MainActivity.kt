2e43ef236431a89596728670cd96b02f4949a35d
Dawid Owoc <dawidowoc6@gmail.com>
2021-12-27 10:16:49 +0100

    Initial commit

---
+++ app/src/main/java/com/github/dawidowoc/quotesapp/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.github.dawidowoc.quotesapp
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
